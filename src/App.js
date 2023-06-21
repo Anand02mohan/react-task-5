b4b900f84ffa135ff1a15c32950f5f3403a43cf6
Anand02mohan <anand.mohan2020@vitstudent.ac.in>
2023-06-21 08:44:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AgeCalculator from './AgeCalculator';
+
+const App = () => {
+  return (
+    <div>
+      <AgeCalculator />
+    </div>
+  );
+};
+
+export default App;
+
